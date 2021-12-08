e9e148674df272da94b0e1be741c5b0e6df85909
Victoria <victorialiu38@gmail.com>
2021-12-08 03:31:54 -0500

    minimap

---
+++ Final_Project_COMP396/Assets/Scripts/Minimap.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Minimap : MonoBehaviour
+{
+    public GameObject mainPlayer;
+    GameObject[] playersArray;
+    public PlayerController playerController;
+
+    void Start()
+    {
+
+    }
+    void LateUpdate()
+    {
+        playersArray = GameObject.FindGameObjectsWithTag("Player");
+
+        foreach (GameObject player in playersArray)
+        {
+            if (player.name == "Player(Clone)")
+            {
+                mainPlayer = player;
+                Vector3 newPosition = mainPlayer.transform.position;
+                newPosition.y = transform.position.y;
+                transform.position = newPosition;
+
+                transform.rotation = Quaternion.Euler(90f, mainPlayer.transform.eulerAngles.y, 0f);
+
+            }
+        }
+
+    }
+}
